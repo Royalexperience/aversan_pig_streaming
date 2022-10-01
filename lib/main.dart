72b9e70ee6e3ea021996a1e7ebcc6492c9062c95
Fabrizio Avolio <f.avolio2000@gmail.com>
2022-10-01 12:49:39 +0200

    TEST CI ANDROID

--- lib/main.dart
+++ lib/main.dart
@@ -7,14 +7,13 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
-        //
+        //ciao
         // Try running your application with "flutter run". You'll see the
         // application has a blue toolbar. Then, without quitting the app, try
         // changing the primarySwatch below to Colors.green and then invoke
@@ -22,7 +21,7 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Color.fromARGB(255, 16, 51, 80),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
