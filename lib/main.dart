4757da840c0f095f5ec537c37a17a3d52f78aa9e
Fabrizio Avolio <f.avolio2000@gmail.com>
2022-10-01 17:59:57 +0200

    ERROR CI TEST

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.green,
+        primarySwatch: Color.fromARGB(10, 10, 10, 0),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
