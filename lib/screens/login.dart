2d675ab476e28c4e17a58e1def88a87e4af77310
Vincenzo Iannucci <34918941+enzo2605@users.noreply.github.com>
2022-10-02 17:28:17 +0200

    add pig hd

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -13,7 +13,7 @@ class LoginPage extends StatelessWidget {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          Image.asset('assets/pig.png'),
+          Image.asset('assets/pig_Hd.png'),
           Container(
             margin: EdgeInsets.all(10),
             child: Center(
