96416eafac76c603aee08a350c71ab848b38939c
Vincenzo Iannucci <34918941+enzo2605@users.noreply.github.com>
2022-10-03 21:23:43 +0200

    Added helping functions

--- lib/widgets/rounded_button.dart
+++ lib/widgets/rounded_button.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: file_names
 import 'package:aversan_pig_streaming/constants/font_sizes.dart';
+import 'package:aversan_pig_streaming/constants/themes/helping_functions.dart';
 import 'package:flutter/material.dart';
 
 class RoundedButton extends StatelessWidget {
@@ -12,11 +13,8 @@ class RoundedButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    double width = MediaQuery.of(context).size.width;
-    double yourWidth = width * 0.85;
-
     return SizedBox(
-        width: yourWidth,
+        width: screenWidthPercentage(context, percentage: 0.85),
         height: 50,
         child: ElevatedButton(
           style: ElevatedButton.styleFrom(
