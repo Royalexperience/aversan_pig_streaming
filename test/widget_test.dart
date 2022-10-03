7d53c81e3a01987e00e1650bfd9325ceac599a2c
Vincenzo Iannucci <34918941+enzo2605@users.noreply.github.com>
2022-10-03 22:50:42 +0200

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,5 +5,21 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-void main() {
+import 'package:aversan_pig_streaming/screens/login.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+Future<void> main() async {
+  Widget testWidget = MediaQuery(
+      data: MediaQueryData(),
+      child: MaterialApp(home: LoginPage())
+  );
+
+  testWidgets('Login Page smoke test', (WidgetTester tester) async {
+
+  await tester.pumpWidget(testWidget);
+
+  await tester.pumpAndSettle();
+
+  });
 }
