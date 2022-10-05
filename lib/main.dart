1555794b2e7943e7f2342fa46e8cfa91fad56751
Vincenzo Iannucci <34918941+enzo2605@users.noreply.github.com>
2022-10-05 21:26:17 +0200

    try to change color scrolling bar

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,8 @@ ThemeData _darkTheme() {
   return ThemeData(
     scaffoldBackgroundColor: DARK_BACKGROUND_COLOR,
     fontFamily: 'Coolvetica',
+    scrollbarTheme: ScrollbarThemeData(
+      thumbColor: MaterialStateProperty.all(MAIN_PINK)),
     /*
     textTheme: const TextTheme(
       headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
