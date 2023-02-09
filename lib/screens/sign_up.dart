1d368d5d90e47ee36bc3f51b77567d790c1e99b7
Vincenzo Iannucci <34918941+enzo2605@users.noreply.github.com>
2023-02-09 18:40:57 +0100

    Update sign_up.dart

--- lib/screens/sign_up.dart
+++ lib/screens/sign_up.dart
@@ -1,5 +1,6 @@
 import 'package:aversan_pig_streaming/constants/font_sizes.dart';
 import 'package:aversan_pig_streaming/constants/themes/helping_functions.dart';
+import 'package:aversan_pig_streaming/constants/margins.dart';
 import 'package:aversan_pig_streaming/screens/sign_in.dart';
 import 'package:aversan_pig_streaming/widgets/circles_in_login_page.dart';
 import 'package:aversan_pig_streaming/widgets/custom_text_field.dart';
@@ -30,56 +31,8 @@ class SignUpPage extends StatelessWidget {
               children: [
                 // Bottone per selezionare l'immagine avatar
                 ImagePickerButton(ImagePickScreen()),
-                // Text field dell'email
-                CustomTextFieldWithIcon(
-                    EMAIL, 
-                    GREY, 
-                    MAIN_PINK, 
-                    2, 
-                    EMAIL_ICON, 
-                    MAIN_BLACK
-                ),
-                // Text field dell'username
-                CustomTextFieldWithIcon(
-                    USERNAME, 
-                    GREY, 
-                    MAIN_PINK, 
-                    2,
-                    ACCOUNT_CIRCLE_ICON, 
-                    MAIN_BLACK
-                ),
-                // Text field della password
-                CustomTextFieldWithIcon(
-                    PASSWORD,
-                    GREY,
-                    MAIN_PINK,
-                    2,
-                    obscureTextFlag: true,
-                    LOCK_OUTLINED_ICON,
-                    MAIN_BLACK
-                ),
-                // Text field del conferma password
-                CustomTextFieldWithIcon(
-                    CONFIRM_PASSWORD_ITALIAN,
-                    GREY,
-                    MAIN_PINK,
-                    2,
-                    obscureTextFlag: true,
-                    LOCK_ICON,
-                    MAIN_BLACK
-                ),
-                // Bottone di invio modulo
-                Container(
-                  margin: EdgeInsets.only(
-                      top: screenHeightPercentage(context, percentage: 0.015)
-                  ),
-                  child: RoundedButton(
-                      SIGN_UP_TEXT_ITALIAN, 
-                      () {}, 
-                      MAIN_PINK, 
-                      WHITE
-                  ),
-                ),
+                // Form di registrazione
+                SignUpForm(),
                 // Pulsante "Hai già un account?"
                 Container(
                   margin: EdgeInsets.only(
@@ -110,6 +63,74 @@ class SignUpPage extends StatelessWidget {
             ),
           ),
         ),
+      ),
+    );
+  }
+}
+
+// Crea il widget del form di accesso
+class SignUpForm extends StatefulWidget {
+  const SignUpForm({super.key});
+
+  @override
+  SignUpFormState createState() {
+    return SignUpFormState();
+  }
+}
+
+// Crea una classe State corrispondente. 
+// Questa classe contiene i dati relativi al modulo.
+class SignUpFormState extends State<SignUpForm> {
+  // Crea una chiave globale che identifichi in modo univoco il widget Modulo
+  // e consente la validazione del form.
+  final _formKey = GlobalKey<FormState>();
+
+  void _validateForm() {
+    // Validate restituisce true se il form è valido, false in caso contrario.
+    if (_formKey.currentState!.validate()) {
+      // Se il modulo è valido, visualizza uno snackbar. Nel mondo reale,
+      // chiameresti spesso un server o salveresti le informazioni in un database.
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Processing Data')),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // Costruisci un widget Modulo utilizzando _formKey creato sopra.
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          // Text field dell'email
+          Container(
+            margin: EdgeInsets.only(bottom: marginSmall(context)),
+            child: EmailTextFormFieldWithIcon(EMAIL, GREY, MAIN_PINK, 2, Icons.email, MAIN_BLACK),
+          ),
+          // Text field dell'username
+          Container(
+            margin: EdgeInsets.only(bottom: marginSmall(context)),
+            child: CustomTextFieldWithIcon(USERNAME, GREY, MAIN_PINK, 2, ACCOUNT_CIRCLE_ICON, MAIN_BLACK),
+          ),
+          // Text field della password
+          Container(
+            margin: EdgeInsets.only(bottom: marginSmall(context)),
+            child: CustomTextFieldWithIcon(PASSWORD, GREY, MAIN_PINK, 2, obscureTextFlag: true, LOCK_OUTLINED_ICON, MAIN_BLACK),
+          ),
+          // Text field del conferma password
+          Container(
+            margin: EdgeInsets.only(bottom: marginSmall(context)),
+            child: CustomTextFieldWithIcon(CONFIRM_PASSWORD_ITALIAN, GREY, MAIN_PINK, 2, obscureTextFlag: true, LOCK_ICON, MAIN_BLACK),
+          ),
+          // Bottone di invio modulo
+          Container(
+            margin: EdgeInsets.only(top: marginSmall(context)),
+            child: RoundedButton(SIGN_UP_TEXT_ITALIAN, _validateForm, MAIN_PINK, WHITE),
+          ),
+        ],
       ),
     );
   }
